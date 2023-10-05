ab1dc402dd942e509e8c0df5fa6f257f32b39e5c
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-05 14:19:21 +0200

    Add project files.

---
+++ 12.1Adding Numbers to a List Box/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Dim counter As Integer
+		For counter = 1 To 7
+			'MsgBox("Mindworx") for the user to print mindwor
+			ListBox1.Items.Add(counter)
+			If counter = 6 Then
+				Exit For
+
+			End If
+		Next
+	End Sub
+End Class
